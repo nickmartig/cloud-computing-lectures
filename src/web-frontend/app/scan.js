ec83ea9fbd8040772870aa452eac9e839ef87daf
nickmartig <nick.martig@stud.hslu.ch>
2024-10-24 14:02:23 +0200

    Update scan.js

--- src/web-frontend/app/scan.js
+++ src/web-frontend/app/scan.js
@@ -1,5 +1,5 @@
 window.addEventListener("DOMContentLoaded", () => {
-    const websocket = new WebSocket("ws://python-websocket-server-student-alex-studer.mycluster-eu-de-1-244439-975dd9665934b81fa7342475b7855171-0000.eu-de.containers.appdomain.cloud");
+    const websocket = new WebSocket("ws:https://python-websocket-server-student-alex-studer.mycluster-eu-de-1-244439-975dd9665934b81fa7342475b7855171-0000.eu-de.containers.appdomain.cloud");
   
     document.querySelector(".minus").addEventListener("click", () => {
       websocket.send(JSON.stringify({ action: "minus" }));
